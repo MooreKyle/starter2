45b818802c5eab911f0957b15061ed1d7d68edb6
Kyle Moore <moorekyle2022@fau.edu>
2023-10-01 00:00:50 -0400

    App Completed! - Step 3 part 3 of 3 Completed, tested, and working! - Added Alert of introduction message to simulation so that user knows via ios UI that app works properly via alert system in ios (alert version, not just console output, works as well in this part). No errors occurred during this part of step 3.

--- starter2/ViewController.swift
+++ starter2/ViewController.swift
@@ -29,17 +29,29 @@ class ViewController: UIViewController {
        // In our example that is whether it is first, second, third or forth
        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
        
-       // Creating a constant of type string that holds an introduction. The introduction receives the values from the outlet connections.
+        // Creating a variable of type string, that holds an introduction. The introduction interpolates the values from the text fields provided.
+        // Currently we can only present the information in a print statement. However, this lets us verify that our app is printing out what is intended!
        let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). \nI am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. \nIt is \(morePetsSwitch.isOn) that I want more pets."
+        
+       // Creates the alert where we pass in our message, which our introduction.
+       let alertController = UIAlertController(title: "My Introduction", message: introduction, preferredStyle: .alert)
+                
+       // A way to dismiss the box once it pops up
+       let action = UIAlertAction(title: "Nice to meet you!", style: .default, handler: nil)
+                
+       // Passing this action to the alert controller so it can be dismissed
+       alertController.addAction(action)
+                
+       present(alertController, animated: true, completion: nil)
        
        print(introduction)
    }
     
-    //Function: stepperDidChange - Used when user interacts with a UIStepper element in the app. Updates numberOfPetsLabel to display current value of the stepper
-    @IBAction func stepperDidChange(_ sender: UIStepper) {
+   //Function: stepperDidChange - Used when user interacts with a UIStepper element in the app. Updates numberOfPetsLabel to display current value of the stepper
+   @IBAction func stepperDidChange(_ sender: UIStepper) {
         print("Stepper changed!")
         numberOfPetsLabel.text = "\(Int(sender.value))"
-    }
+   }
 
 }
 
