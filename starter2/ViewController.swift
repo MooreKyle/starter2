cedbc11ff5be84cd90f734d92a5478b5d1e87e1d
Kyle Moore <moorekyle2022@fau.edu>
2023-09-30 22:40:27 -0400

    Step 2 - Part 2 of 3 Completed, tested, and working - Prints student introduction and additional info to console output based on info entered on simulation. Like previous step in Step 2, part 1 of 3, the linking was not correct and had the same issue.

--- starter2/ViewController.swift
+++ starter2/ViewController.swift
@@ -22,6 +22,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+       
+       print("Introduce button tapped!")
+       // Lets us choose the title we have selected from the segmented control
+       // In our example that is whether it is first, second, third or forth
+       let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
+       
+       // Creating a constant of type string that holds an introduction. The introduction receives the values from the outlet connections.
+       let introduction = "My name is \(firstNameTextField.text!) \(lastNameTextField.text!) and I attend \(schoolNameTextField.text!). \nI am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. \nIt is \(morePetsSwitch.isOn) that I want more pets."
+       
+       print(introduction)
+   }
+    
     //Function: stepperDidChange - Used when user interacts with a UIStepper element in the app. Updates numberOfPetsLabel to display current value of the stepper
     @IBAction func stepperDidChange(_ sender: UIStepper) {
         print("Stepper changed!")
